bdc42050fcaa7587f0253373d75ddcf3cf4ad8d2
yoon <58201514+onlyoon@users.noreply.github.com>
2023-04-27 02:51:07 +0000

    Update PaymentRecord.js and PaymentRecord.module.css

--- src/components/ContainerComponents/NetworkManager/PaymentRecord/PaymentRecord.js
+++ src/components/ContainerComponents/NetworkManager/PaymentRecord/PaymentRecord.js
@@ -1,12 +1,25 @@
 import classes from "./PaymentRecord.module.css";
 
 const PaymentRecord = () => {
-  return 
-  <div className={classes.paymentrecord_wrap}>
-    <div className={classes.paymentrecord}>
-      PaymentRecord
+  return (
+    <div className={classes.paymentrecord_wrap}>
+      <div className={classes.paymentrecord}>
+        <div className={classes.paymentrecord_text}>결제내역</div>
+        <div className={classes.paymentrecord_table_wrap}>
+          <div className={classes.check_table}>
+            <div className={classes.check_table_text}>결제 기간</div>
+            <div className={classes.date_bar}>
+              <input type={"date"} />
+              <div>~</div>
+              <input type={"date"} />
+            </div>
+            <button className={classes.check_table_button}>조회</button>
+          </div>
+          <div>Table</div>
+        </div>
+      </div>
     </div>
-  </div>;
+  );
 };
 
 export default PaymentRecord;
